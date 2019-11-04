e87091ad35cbe952cb1bfab5d73cc908ba25da79
Vamsi Dharmaji <vamsi.dharmaji@outlook.com>
2019-11-04 16:04:55 -0500

    Update app.component.spec.ts

--- apps/ngdemo/src/app/app.component.spec.ts
+++ apps/ngdemo/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Lib1Module } from '@ngdev/lib1';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent]
+      declarations: [AppComponent],
+      imports: [Lib1Module]
     }).compileComponents();
   }));
 
